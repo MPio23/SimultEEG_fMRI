a9a79e437aa026773fa6482dbd78790fde1de06a
MPio23 <marek.piorecky@gmail.com>
2020-09-29 09:42:53 +0200

    clear comments

--- freq_stat/ALL_CHAN_ALL_FREQ_grand.m
+++ freq_stat/ALL_CHAN_ALL_FREQ_grand.m
@@ -3,7 +3,7 @@ clear all;
 clc;
 
 ft_defaults
-FilePathT=('D:\Marek_P\256EEG_dp\freq_analysis\');
+FilePathT=('D:XXXXXXXXX\');
 FileNameT = dir([FilePathT '*Relfreq_T*.mat']);
 nFilesT = length(FileNameT);
 
@@ -20,7 +20,7 @@ cfg.neighbourdist = 10;
 neighbours = ft_prepare_neighbours(cfg);
 ft_neighbourplot(cfg);
 
-for i = 1:1%nFilesT
+for i = 1:nFilesT
     
     data = load([FilePathT FileNameT(i).name]);
     name = FileNameT(i).name(4:end-4);
@@ -34,13 +34,13 @@ for i = 1:1%nFilesT
     GA_freq_A = ft_freqgrandaverage(cfg, datpre{:});
     
     cfg = [];
-    cfg.channel          = 'all';%{'E1','E2','E3'};
+    cfg.channel          = 'all';
     cfg.neighbours       = neighbours; 
     cfg.latency          = 'all';
-    cfg.avgoverchan      = 'no'; %prumer pres kanaly
+    cfg.avgoverchan      = 'no'; 
     cfg.frequency        = 'all';
     cfg.parameter        = 'powspctrm';
-    cfg.tail             = 0; %oboustranny test (neptam se vetsi mensi)
+    cfg.tail             = 0; 
     cfg.method           = 'montecarlo';
     cfg.correctm         = 'cluster';
     cfg.correcttail      = 'prob';
@@ -48,7 +48,7 @@ for i = 1:1%nFilesT
     cfg.numrandomization = 500;
     cfg.ivar = 2;
 
-    %nadefinovani, co je A a T
+
     design = zeros(2,2);
     design(1,:) = 1;
     design(2,:) = [1,2];
@@ -59,22 +59,22 @@ for i = 1:1%nFilesT
 
 end
 
-%%
+
     cfg = [];
-    cfg.channel          = 'all';%{'E1','E2','E3'};
+    cfg.channel          = 'all';
     cfg.neighbours       = neighbours; 
     cfg.latency          = 'all';
-    cfg.avgoverchan      = 'no'; %prumer pres kanaly
+    cfg.avgoverchan      = 'no'; 
     cfg.frequency        = 'all';
     cfg.parameter        = 'powspctrm';
-    cfg.tail             = 0; %oboustranny test (neptam se vetsi mensi)
+    cfg.tail             = 0; 
     cfg.method           = 'montecarlo';
     cfg.correctm         = 'cluster';
     cfg.correcttail      = 'prob';
     cfg.statistic        = 'ft_statfun_indepsamplesT'; 
     cfg.numrandomization = 1000;
 
-    %nadefinovani, co je A a T
+
     design = zeros(1,size(Normalized_freqERF_T.(pac{i}).freq.powspctrm,1) + size(Normalized_freqERF_A.(pac{i}).freq.powspctrm,1));
     design(1,1:size(Normalized_freqERF_T.(pac{i}).freq.powspctrm,1)) = 1;
     design(1,(size(Normalized_freqERF_T.(pac{i}).freq.powspctrm,1)+1):(size(Normalized_freqERF_T.(pac{i}).freq.powspctrm,1)+...
@@ -89,7 +89,7 @@ end
 
 save Statistic stat
 
-%%
+
 cfg.elec = ft_read_sens('GSN-HydroCel-257.sfp');
 cfg.layout = ft_prepare_layout(cfg);
 % scatter3(cfg.layout.pos(:,1),cfg.layout.pos(:,2), ones(1,262))
@@ -99,9 +99,9 @@ x = cfg.layout.pos(:,1);
 y = cfg.layout.pos(:,2);
 z = ones(1,262);
 
-for i = 1:length(pac) %pres vsechny pacienty
+for i = 1:length(pac) 
      figure(i)
-   for j = 1:size(stat.(pac{i}).freq.prob,2) %pro 17 dilku po 1 Hz
+   for j = 1:size(stat.(pac{i}).freq.prob,2)
                  
         statDif1 = find(stat.(pac{i}).freq.prob(:,j)<0.5);
         scatter3(x(statDif1),y(statDif1),j*ones(1,size(statDif1,1)),[],'r','filled')
